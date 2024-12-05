738774b5fc7fb66746561888a9eb8f0749288679
Lars <laarrss@protonmail.com>
2024-12-05 09:52:08 +0100

    routing setup

--- src/template.jsx
+++ src/template.jsx
@@ -2,9 +2,33 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./template.module.css";
+import { useRoutes } from "react-router-dom";
+import Frontpage from "./pages/frontpage/frontpage.jsx";
+import Faqpage from "./pages/faq/faq.jsx";
+import CustomerClub from "./pages/customerClub/customerClub.jsx";
+import ProductsPage from "./pages/products/products.jsx";
 
 function App() {
-  return <></>;
+  let pagesRoute = useRoutes(
+    {
+      path: "/",
+      element: <Frontpage />,
+    },
+    {
+      path: "/faq",
+      element: <Faqpage />,
+    },
+    {
+      path: "/club",
+      element: <CustomerClub />,
+    },
+    {
+      path: "/products",
+      element: <ProductsPage />,
+    }
+  );
+
+  return <>{pagesRoute}</>;
 }
 
 export default App;
