8bd947b8388d3692c7872f78f7a31991ef207842
Lars <laarrss@protonmail.com>
2025-04-21 01:06:20 +0200

    nameændring

--- src/comps/singleQuote/singleQuote.jsx
+++ src/comps/singleQuote/singleQuote.jsx
@@ -1,16 +1,14 @@
-import styles from "./singleQuote.module.css";
+import styles from "./SingleQuote.module.css";
 
 /* Et enkelt citat fra kunderne. */
-export default function singleQUote({obj}) {
-    return <>
-    <div className={styles.box}>
-    <p className={styles.desc}>
-        {`"${obj.description}"`}
-    </p>
+export default function singleQUote({ obj }) {
+  return (
+    <>
+      <div className={styles.box}>
+        <p className={styles.desc}>{`"${obj.description}"`}</p>
 
-    <p className={styles.name}>{`- ${obj.name}`}</p>
-    </div>
-
-
+        <p className={styles.name}>{`- ${obj.name}`}</p>
+      </div>
     </>
+  );
 }
